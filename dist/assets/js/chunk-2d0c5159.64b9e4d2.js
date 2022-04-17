2f9992161f6b2db5cd1a7f814b796204bfcd409e
lunnar-being <chaf@pku.edu.cn>
2022-04-17 15:14:36 +0800

    change axios

--- dist/assets/js/chunk-2d0c5159.64b9e4d2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c5159"],{"3e1b":function(e,n,a){"use strict";a.r(n);var t=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("div",[a("a-rate",{model:{value:e.value,callback:function(n){e.value=n},expression:"value"}})],1)},u=[],l={data:function(){return{value:2,dates:new Date}}},c=l,r=a("2877"),o=Object(r["a"])(c,t,u,!1,null,null,null);n["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d0c5159.64b9e4d2.js.map
